0703d3553ad971d5543dcccbf69610d2d88661c8
rutabageldev <187210498+rutabageldev@users.noreply.github.com>
2025-07-08 00:14:32 +0000

    Pruned deprecated file

--- services/account_registry.py
+++
@@ -1,8 +0,0 @@
-# services/account_registry.py
-import yaml  # type: ignore
-import os
-
-_registry_path = os.path.join(os.path.dirname(__file__), "account_registry.yaml")
-
-with open(_registry_path, "r") as f:
-    SUPPORTED_ACCOUNTS = yaml.safe_load(f)
